3c9da52a48ea6bd2b802c7975b6200bef2cd53bf
Feras Barahmeh <ferasbarahmeh55@gmail.com>
2024-02-17 17:06:50 +0300

    add new design status tr (pulse)

--- resources/views/components/backend/status.blade.php
+++ resources/views/components/backend/status.blade.php
@@ -1,13 +1,21 @@
 @php use App\Enums\Status; @endphp
-@props(['status'])
+@props(['status', 'style' => 'pulse'])
 
+@php
+    $type = $status === Status::Active->value ? 'success' : 'danger';
+@endphp
 
-@if($status === Status::Active->value)
-    <div class="relative">
-        <span class="badge badge-pill badge-success text-capitalize">{{ $status }}</span>
+@if($style === 'pulse')
+    <div class="text-cente  position-relative">
+            <span class="text-{{ $type }} text-capitalize position-relative">
+                <span class="{{ $type === 'success' ? 'pulse' : 'pulse-danger' }}" @style(['top: 50%; left: -8px; transform: translate(-50%, -50%);'])></span>
+                {{ $status }}
+            </span>
     </div>
-@elseif($status === Status::InActive->value)
-    <div class="relative">
-        <span class="badge badge-danger text-capitalize">{{ $status }}</span>
+@elseif($style==='badge')
+    <div class="text-cente ">
+            <span class="badge badge-{{ $type === 'success' ? 'pill' : 'danger' }} badge-success text-capitalize">
+                {{ $status }}
+            </span>
     </div>
 @endif
