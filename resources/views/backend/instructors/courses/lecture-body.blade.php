df08ce4fb018a144c99d1a11cfcd028bc9e47002
Feras Barahmeh <ferasbarahmeh55@gmail.com>
2024-03-16 13:51:02 +0300

    add tooltip to lecture name

--- resources/views/backend/instructors/courses/lecture-body.blade.php
+++ resources/views/backend/instructors/courses/lecture-body.blade.php
@@ -1,4 +1,4 @@
-<x-collapse-body id="content-lecture-{{ $lecture->id }}">
+<x-collapse-body id="content-lecture-{{ $lecture->id }}" class="border-bottom">
     <div class="pl-3 pr-3">
         <div class="mt-2">
             <p class="text-muted">{{ $lecture->description }}</p>
--- resources/views/livewire/courses/lecture.blade.php
+++ resources/views/livewire/courses/lecture.blade.php
@@ -1,8 +1,8 @@
 <div>
-    <div class="d-flex justify-content-between align-items-center">
+    <div class="d-flex justify-content-between align-items-center" >
         <x-collapse-button href="content-lecture-{{ $lecture->id }}" class="text-dark mt-4 d-flex align-items-center gap-5">
             <i class="fa fa-{{ $lecture->published ?  'fire text-orange' : 'eye-slash text-warning' }} tx-10"></i>
-            <span>{{ $lecture->lecture_order }} - {{  $lecture->name }}</span>
+            <span data-toggle="tooltip" title="" data-placement="top" data-original-title="{{ $lecture->published ? 'published' : 'private' }}">{{ $lecture->lecture_order }} - {{  $lecture->name }}</span>
         </x-collapse-button>
 
         @include('backend.instructors.courses.lecture-options')
