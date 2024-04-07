54d4ca2d88b39998c2cad06cac15c4e9c6faa4e5
Feras Barahmeh <ferasbarahmeh55@gmail.com>
2024-04-08 02:33:03 +0300

    fix EmailVerificationTest.php

--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -16,6 +16,7 @@ class EmailVerificationTest extends TestCase
 
     public function test_email_verification_screen_can_be_rendered(): void
     {
+        $this->withOutLaravelLocalizationMiddlewares();
         $user = User::factory()->create([
             'email_verified_at' => null,
         ]);
@@ -27,6 +28,7 @@ class EmailVerificationTest extends TestCase
 
     public function test_email_can_be_verified(): void
     {
+        $this->withOutLaravelLocalizationMiddlewares();
         $user = User::factory()->create([
             'email_verified_at' => null,
         ]);
@@ -41,9 +43,9 @@ class EmailVerificationTest extends TestCase
 
         $response = $this->actingAs($user)->get($verificationUrl);
 
-        Event::assertDispatched(Verified::class);
+        Event::assertDispatched(\Illuminate\Auth\Events\Verified::class);
         $this->assertTrue($user->fresh()->hasVerifiedEmail());
-        $response->assertRedirect(RouteServiceProvider::HOME.'?verified=1');
+        $response->assertRedirect(RouteServiceProvider::determineIntendedHome().'?verified=1');
     }
 
     public function test_email_is_not_verified_with_invalid_hash(): void
